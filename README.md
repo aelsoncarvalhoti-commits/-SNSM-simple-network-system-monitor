c3498017b1377d1ff5a3792d2462c88bc76076d6
Aelson <aelsoncarvalho.ti@gmail.com>
2025-10-19 02:25:47 -0300

    Adicionando arquivos de informações de sistema e de log

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# SNSM simple network system monitor
+# SNSM Cliente simple network system monitor
+
+o SNSM é um sistema para monitorar uso dos computadores em rede em pequenas empresas como CPU, memoria e disco alem de executar comandos especificos como testes de velocidade de internet.
+Funcionará sem o servidor e será mostrado os dados em um frontend utilizando a biblioteca tkinter ou enviará os dados para um servidor que tambem será desenvolvido em flask.
+
+# Basic features / Funcionalidades básicas
+
+- [ ] Local frontend / frontend local (tkinter)
+- [ ] server integration / integração com servidor (flask)
+- [x] System informations / Informações do sistema
+- [ ] CPU information / Informação da CPU
+- [ ] RAM information / Informação da memória RAM
+- [ ] Disc information / Informação de disco
+- [ ] Internet speed test / Teste de velocidade de internet
+
+# Others features (future) / Outras funcionalidades (futuro)
+
+Assim que novas funcionalidades forem sendo adicionadas seŕa colocada na lista nesse readme
--- app.py
+++ app.py
@@ -8,41 +8,49 @@ on_off = True
 
 t_wait = 15
 
+'''
+    Parte responsável por gerar log de informação do sistema.
+    por padrão ela so irá executar uma vez quando o app é executado
+'''
 partitions = {}
 
+# Pega as unidades de disco e salva na variável partition
 for p in psutil.disk_partitions(all=False):
-    partitions[p.mountpoint] = f'{psutil.disk_usage(p.mountpoint).total/(1024**3):.2f}'
+    partitions[p.mountpoint] = f'{psutil.disk_usage(p.mountpoint).total/(1024**3):.2f}GB Total | {psutil.disk_usage(p.mountpoint).used/(1024**3):.2f}GB'
 
+# Gera o arquivo de informação do sistema
 with open('system_infor.json', 'w') as system_infor:
     system_infor.writelines(
         json.dumps({
-            'Sistem': f'Sistem: {platform.system()} {platform.release()}',
-            'Arquitecture': f'{platform.machine()}',
-            'Total of cores': f'{psutil.cpu_count(logical=False)}',
-            'Total of threads': f'{psutil.cpu_count(logical=True)}',
-            'Memory total': f'{psutil.virtual_memory().total / (1024**3):.2f}GB', 
-            'Discs': partitions      
+            'Sistem': f'Sistem: {platform.system()} {platform.release()}', # Sistema operacional e sua versão
+            'Arquitecture': f'{platform.machine()}', # Arquitetura do sistema
+            'Total of cores': f'{psutil.cpu_count(logical=False)}', # numero de núcleos físicos do processador
+            'Total of threads': f'{psutil.cpu_count(logical=True)}', # Numero de threads do processador
+            'Memory total': f'{psutil.virtual_memory().total / (1024**3):.2f}GB', # Quantidade total de memória do sistema
+            'Discs': partitions # pega a variavel partitions e salva os discos     
                     })
     )
 
 while on_off:
-    with open('log_file.txt', '+a') as log_file:
-        log_file.writelines('teste')
-    #cpu usage
-    print(f'CPU usage porcentage: {psutil.cpu_percent(interval=1)}% ')
-    print(f'Memory total: {psutil.virtual_memory().total / (1024**3):.2f}GB')
-    #memory usage
-    print(f'Memory usage porcentage: {psutil.virtual_memory().percent}%')
-
-    print(f'Memory used: {psutil.virtual_memory().used / (1024**3):.2f}GB')
-
-    print(f"Sistem: {platform.system()} {platform.release()}")
-    print(f"Arquitetura: {platform.machine()}")
-    print(f"Total of cores: {psutil.cpu_count(logical=False)}")
-    print(f"Total of threads: {psutil.cpu_count(logical=True)}")
-
-    print(f'{psutil.disk_usage('/').total/(1024**3):.2f}')
-
     net = psutil.net_io_counters()
-    print(f"Bytes enviados: {net.bytes_sent}, Bytes recebidos: {net.bytes_recv}")
+
+    data = {
+                'CPU usage porcentage': f'{psutil.cpu_percent(interval=1)}%',
+                'Memory usage porcentage:': f'{psutil.virtual_memory().percent}%',
+                'Total of memory used': f'{psutil.virtual_memory().used / (1024**3):.2f}GB',
+                'Internet bytes enviados': net.bytes_sent,
+                'Internet bytes recebidos': net.bytes_recv
+            }
+    
+    try:
+        with open('log_file.json', 'r') as log_file:
+            logs = json.load(log_file)
+    except(json.JSONDecodeError, FileNotFoundError):
+        logs = []
+
+    logs.append(data)
+
+    with open('log_file.json', 'w') as log_file:
+        json.dump(logs, log_file, indent=4)
+    
     sleep(t_wait)
